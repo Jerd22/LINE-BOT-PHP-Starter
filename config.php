ee463d481f4aa1de9315be1befac21c52691f824
Jerd22 <teeo300881@gmail.com>
2017-03-13 15:24:20 +0700

    Update config.php

--- config.php
+++ config.php
@@ -5,13 +5,30 @@
    $credentials = "user=jgweykkxjvumfz password=0f8783f77246707669a93ce2b58ef454b2587beed2a01df40f122817d04ff8d1";
 
    $db = pg_connect( "$host $port $dbname $credentials"  );
-   if(!$db){
+/*  
+if(!$db){
       echo "Error : Unable to open database\n";
    } else {
       echo "Opened database successfully\n";
    }
 
+   $sql =<<<EOF
+      CREATE TABLE COMPANY
+      (ID INT PRIMARY KEY     NOT NULL,
+      NAME           TEXT    NOT NULL,
+      AGE            INT     NOT NULL,
+      ADDRESS        CHAR(50),
+      SALARY         REAL);
+EOF;
 
+   $ret = pg_query($db, $sql);
+   if(!$ret){
+      echo pg_last_error($db);
+   } else {
+      echo "Table created successfully\n";
+   }
+   pg_close($db);
+*/
 //$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 //$server = $url["host"];
